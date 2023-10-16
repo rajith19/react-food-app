988e7f1dd0f3bb7d7e923eaa6d413fb307c25325
rajith27 <rajithvgopalm@gmail.com>
2023-10-16 15:04:57 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "H1 tag it is"),
+        React.createElement("h2", {}, "h2 tag it is")
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "H1 tag it is"),
+        React.createElement("h2", {}, "h2 tag it is")
+    ])
+]
+
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);  
