41e8eba29af66fc052a13f2d23b18faa75686e48
rajith27 <rajithvgopalm@gmail.com>
2023-10-25 23:00:11 -0400

    updated package and gitignore

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child1" }, [
         React.createElement("h1", {}, "H1 tag it is"),
