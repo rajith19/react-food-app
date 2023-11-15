d672c67c57d94586ea11873609646103e3113355
rajith27 <rajithvgopalm@gmail.com>
2023-11-15 11:48:13 -0500

    added placeholders

---
+++ src/components/ShimmerUi.js
@@ -0,0 +1,64 @@
+const ShimmerUi = () => {
+    return (
+        <div>
+            <div className="shimmer-container">
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card">
+                        {/* Shimmer for card content */}
+                    </div>
+                    <div className="shimmer-card-descriptions">
+                        <div className="shimmer-card-description width-3"></div>
+                        <div className="shimmer-card-description width-3"></div>
+                    </div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card">
+                        {/* Shimmer for card content */}
+                    </div>
+                    <div className="shimmer-card-descriptions">
+                        <div className="shimmer-card-description width-3"></div>
+                        <div className="shimmer-card-description width-3"></div>
+                    </div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card">
+                        {/* Shimmer for card content */}
+                    </div>
+                    <div className="shimmer-card-descriptions">
+                        <div className="shimmer-card-description width-3"></div>
+                        <div className="shimmer-card-description width-3"></div>
+                    </div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card">
+                        {/* Shimmer for card content */}
+                    </div>
+                    <div className="shimmer-card-descriptions">
+                        <div className="shimmer-card-description width-3"></div>
+                        <div className="shimmer-card-description width-3"></div>
+                    </div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card">
+                        {/* Shimmer for card content */}
+                    </div>
+                    <div className="shimmer-card-descriptions">
+                        <div className="shimmer-card-description width-3"></div>
+                        <div className="shimmer-card-description width-3"></div>
+                    </div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card">
+                        {/* Shimmer for card content */}
+                    </div>
+                    <div className="shimmer-card-descriptions">
+                        <div className="shimmer-card-description width-3"></div>
+                        <div className="shimmer-card-description width-3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ShimmerUi;
---
+++ src/components/ShimmerUiDetails.js
@@ -0,0 +1,20 @@
+const ShimmerUiDetails = () => {
+    return (
+        <div>
+            <div className="shimmer-container">
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card">
+                        {/* Shimmer for card content */}
+                    </div>
+                    <div className="shimmer-card-descriptions">
+                        <div className="shimmer-card-description width-3"></div>
+                        <div className="shimmer-card-description width-2"></div>
+                        <div className="shimmer-card-description width-2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ShimmerUiDetails;
