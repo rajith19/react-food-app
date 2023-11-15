279559f8a6c6865a9a89567674f62357bd9a49c6
rajith27 <rajithvgopalm@gmail.com>
2023-11-15 11:47:42 -0500

    added error page

---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
+const Error = () => {
+
+    const err = useRouteError();
+    return (
+        <div><h1>Something Went Wrong</h1>
+            <h3>{err.status} : {err.statusText}</h3>
+        </div>
+    )
+}
+
+export default Error;
