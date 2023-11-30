89601d099004a048fcd1a9b0bcdf297102927554
rajith27 <rajithvgopalm@gmail.com>
2023-11-30 12:53:39 -0500

    add classname and context

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,28 @@
-import { useEffect, useState } from "react";
-import { LOGO_URL } from "../utils/constants";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../utils/UserContext";
+import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
 
     const [btnName, setBtnName] = useState("Login");
+    const { loggedInUser, setName } = useContext(UserContext)
 
     return (
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src={LOGO_URL} />
+        <div className="flex justify-between shadow-lg">
+            <div className="px-10">
+                <img className="w-36" src={LOGO_URL} />
             </div>
-            <div className="nav-items">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About Us</Link></li>
-                    <li><Link to="/contact">Contact Us</Link></li>
-                    <li><Link to="/grocery">Grocery</Link></li>
-                    <li><Link to="/cart">Cart</Link></li>
-                    <button className="login" onClick={() => {
+            <div className="flex items-center px-10">
+                <ul className="flex">
+                    <li className="px-4"><Link to="/">Home</Link></li>
+                    <li className="px-4"><Link to="/about">About Us</Link></li>
+                    <li className="px-4"><Link to="/contact">Contact Us</Link></li>
+                    <li className="px-4"><Link to="/grocery">Grocery</Link></li>
+                    <li className="px-4"><Link to="/cart">Cart</Link></li>
+                    <button className="px-4" onClick={() => {
                         btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
-                    }}>{btnName}</button>
+                    }}>{btnName + " " + loggedInUser}</button>
                 </ul>
 
             </div>
