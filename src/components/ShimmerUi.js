1f380e13e3c3aa0a786a2b50c65ba1c75ca534a7
rajith27 <rajithvgopalm@gmail.com>
2024-09-04 23:22:56 -0400

    updated shimmer UI

--- src/components/ShimmerUi.js
+++ src/components/ShimmerUi.js
@@ -1,61 +1,19 @@
 const ShimmerUi = () => {
+
+    const emptyArray = new Array(6).fill(null); // Create an array with 6 empty values
+
     return (
-        <div>
-            <div className="shimmer-container">
-                <div className="shimmer-card-container">
-                    <div className="shimmer-card">
-                        {/* Shimmer for card content */}
+        <div className="mx-20 my-4 px-16">
+            <div className="flex m-4 items-center justify-between m-4 flex-wrap">
+                {emptyArray.map((_, index) => (
+                    <div role="status" className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
+                        <div className="flex items-center justify-center w-full h-64 bg-gray-300 rounded sm:w-96 dark:bg-gray-700 mx-1 my-2">
+                            <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                            </svg>
+                        </div>
                     </div>
-                    <div className="shimmer-card-descriptions">
-                        <div className="shimmer-card-description width-3"></div>
-                        <div className="shimmer-card-description width-3"></div>
-                    </div>
-                </div>
-                <div className="shimmer-card-container">
-                    <div className="shimmer-card">
-                        {/* Shimmer for card content */}
-                    </div>
-                    <div className="shimmer-card-descriptions">
-                        <div className="shimmer-card-description width-3"></div>
-                        <div className="shimmer-card-description width-3"></div>
-                    </div>
-                </div>
-                <div className="shimmer-card-container">
-                    <div className="shimmer-card">
-                        {/* Shimmer for card content */}
-                    </div>
-                    <div className="shimmer-card-descriptions">
-                        <div className="shimmer-card-description width-3"></div>
-                        <div className="shimmer-card-description width-3"></div>
-                    </div>
-                </div>
-                <div className="shimmer-card-container">
-                    <div className="shimmer-card">
-                        {/* Shimmer for card content */}
-                    </div>
-                    <div className="shimmer-card-descriptions">
-                        <div className="shimmer-card-description width-3"></div>
-                        <div className="shimmer-card-description width-3"></div>
-                    </div>
-                </div>
-                <div className="shimmer-card-container">
-                    <div className="shimmer-card">
-                        {/* Shimmer for card content */}
-                    </div>
-                    <div className="shimmer-card-descriptions">
-                        <div className="shimmer-card-description width-3"></div>
-                        <div className="shimmer-card-description width-3"></div>
-                    </div>
-                </div>
-                <div className="shimmer-card-container">
-                    <div className="shimmer-card">
-                        {/* Shimmer for card content */}
-                    </div>
-                    <div className="shimmer-card-descriptions">
-                        <div className="shimmer-card-description width-3"></div>
-                        <div className="shimmer-card-description width-3"></div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
--- src/components/ShimmerUiDetails.js
+++ src/components/ShimmerUiDetails.js
@@ -1,20 +1,26 @@
 const ShimmerUiDetails = () => {
-    return (
-        <div>
-            <div className="shimmer-container">
-                <div className="shimmer-card-container">
-                    <div className="shimmer-card">
-                        {/* Shimmer for card content */}
-                    </div>
-                    <div className="shimmer-card-descriptions">
-                        <div className="shimmer-card-description width-3"></div>
-                        <div className="shimmer-card-description width-2"></div>
-                        <div className="shimmer-card-description width-2"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+	const emptyArray = new Array(10).fill(null);
+
+	return (
+		<>
+			<div className="animate-pulse flex flex-col items-center text-center max-w-3xl mt-10 m-auto p-3">
+				<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-48 mb-2.5"></div>
+				<div className="w-60 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+			</div>
+			<div role="status" className="max-w-3xl p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700 	 mt-10 m-auto p-3">
+				{emptyArray.map((_, index) => {
+					return <div className="flex items-center justify-between pt-4">
+						<div>
+							<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+							<div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+						</div>
+						<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+					</div>
+				})}
+			</div>
+			
+		</>
+	);
+};
 
 export default ShimmerUiDetails;
