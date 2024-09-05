2b83b5071eb6d192e8ff5c76e92ef2a30e4da41a
rajith27 <rajithvgopalm@gmail.com>
2024-09-05 14:36:03 -0400

    optimised code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,91 +1,138 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useDebounce from "../utils/useDebounce";
 import RestaurantCard, { PromotedRestaurantCard } from "./RestaurantCard";
 import ShimmerUi from "./ShimmerUi";
+import UserContext from "../utils/UserContext.js";
 
 const Body = () => {
-
     const [ListOfRestaurants, setListOfRestaurants] = useState([]);
-    const [filteredRestaurants, setfilteredRestaurants] = useState([]);
-    const [searchRestaurantText, setsearchRestaurantText] = useState("");
-    const debouncedSearchTerm = useDebounce(searchRestaurantText, 500); // 500ms delay
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    const [searchRestaurantText, setSearchRestaurantText] = useState("");
+    const [tempName, setTempName] = useState(""); // Temporary state for profile name input
+    const [isFiltered, setIsFiltered] = useState(false); // Temporary state for profile name input
     const WithRestaurantCard = PromotedRestaurantCard(RestaurantCard);
 
+    const { setName } = useContext(UserContext);
+    let debouncedUserName = useDebounce(tempName, 500); // 500ms delay for debouncing the context value
+
     useEffect(() => {
-        if (debouncedSearchTerm) {
-            filteredData();
+        if (debouncedUserName) {
+            // Perform any action here when the debounced username is updated
+            setName(debouncedUserName);
         }
-    }, [debouncedSearchTerm]);
+    }, [debouncedUserName]);
 
     useEffect(() => {
         fetchData();
-    }, [])
+    }, []);
 
     const fetchData = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
-
         const cards = json?.data?.cards;
 
         // Check if the property exists in one of the elements within the range of 0 to 6
         const foundRestaurantList = cards?.slice(0, 7).find(card => {
-            return (card?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+            return card?.card?.card?.gridElements?.infoWithStyle?.restaurants;
         });
         if (foundRestaurantList) {
-            // If found, set the ListOfRestaurants to the value of the property
             setListOfRestaurants(foundRestaurantList?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-            setfilteredRestaurants(foundRestaurantList?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+            setFilteredRestaurants(foundRestaurantList?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         } else {
-            // If not found, set ListOfRestaurants to an empty array or handle it as needed
             setListOfRestaurants([]);
-            setfilteredRestaurants([]);
+            setFilteredRestaurants([]);
         }
-
-    }
+    };
 
     const filteredData = () => {
         const filteredRes = ListOfRestaurants.filter((res) =>
-            (res?.info?.name.toLowerCase().includes(searchRestaurantText.toLowerCase()))
-        )
-        setfilteredRestaurants(filteredRes);
-    }
+            res?.info?.name.toLowerCase().includes(searchRestaurantText.toLowerCase())
+        );
+        setFilteredRestaurants(filteredRes);
+        setIsFiltered(false);
+    };
 
-    return ListOfRestaurants?.length === 0 ? <ShimmerUi /> :
-        (
-            <div className="mx-20 my-4 px-16">
-                <div className="flex m-4 items-center">
-                    <div className="pr-4 py-4">
-                        <input type="text" className="border border-black mr-4 cursor-auto" value={searchRestaurantText}
-                            onChange={(e) => setsearchRestaurantText(e.target.value)}></input>
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded cursor-pointer" onClick={() => {
-                            filteredData()
-                        }}>Search</button>
-                    </div>
-                    <div className="p-4">
-                        <button className="flex items-center bg-white hover:bg-gray-100 text-gray-800 border font-semibold py-[0.3rem] px-4 rounded cursor-pointer" onClick={() => {
-                            const filteredList = ListOfRestaurants.filter(res => res.info.avgRating > 4.3);
-
-                            setfilteredRestaurants(filteredList);
-                        }}> Top Rated Restaurant (Above 4.5 <svg className="w-4 h-4 text-yellow-300 inline-block" ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-    </svg>) </button>
-                    </div>
+    return ListOfRestaurants?.length === 0 ? (
+        <ShimmerUi />
+    ) : (
+        <div className="mx-20 my-4 px-16">
+            <div className="flex m-4 items-center">
+                <div className="pr-4 py-4">
+                    <input
+                        type="text"
+                        className="rounded border border-black mr-4 cursor-auto py-1 px-2"
+                        value={searchRestaurantText}
+                        onChange={(e) => setSearchRestaurantText(e.target.value)}
+                    />
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-lg cursor-pointer"
+                        onClick={filteredData}
+                    >
+                        Search
+                    </button>
                 </div>
-                <div className="grid grid-cols-4 gap-6 grid-flow-row justify-items-center">
-                    {
-                        filteredRestaurants?.map((restaurant) => (
-                            <Link key={restaurant.info.id} to={`/restaurant/${restaurant.info.id}`} className="filteredLink">
-                                {restaurant?.info?.aggregatedDiscountInfoV3?.discountTag === "FLAT DEAL" ?
-                                    <WithRestaurantCard resData={restaurant} key={restaurant.info.id} />
-                                    : <RestaurantCard resData={restaurant} key={restaurant.info.id} />}
-                            </Link>
-                        ))
-                    }
+                <div className="p-4">
+                    <button
+                        className="flex items-center bg-blue-500 hover:bg-blue-700 text-white border font-semibold py-[0.3rem] px-4 rounded-lg cursor-pointer"
+                        onClick={() => {
+                            const filteredList = ListOfRestaurants.filter(res => (res.info.avgRating > 4.5));
+                            setFilteredRestaurants(filteredList);
+                            setIsFiltered(true);
+                        }}
+                    >
+                        Top Rated Restaurant (Above 4.5{" "}
+                        <svg
+                            className="w-4 h-4 text-yellow-300 inline-block"
+                            ariaHidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 22 20"
+                        >
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        )
+                    </button>
+                </div>
+                <div className="p-4">
+                    <input
+                        type="text"
+                        placeholder="Change the profile name"
+                        className="rounded border border-black mr-4 cursor-auto px-2 py-1"
+                        value={tempName}
+                        onChange={(e) => setTempName(e.target.value)} // Updated to use target.value for username change
+                    />
                 </div>
             </div>
-        )
-}
-
+            {isFiltered &&
+                <span class="bg-blue-100 text-white text-xs font-medium me-2 px-2.5 py-1 rounded dark:bg-blue-900 dark:text-white flex items-center w-fit">
+                    Above 4.5{" "}
+                    <svg
+                        className="ml-1 w-4 h-4 text-yellow-300 inline-block"
+                        ariaHidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                    >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                    <button className="ml-2" onClick={() => filteredData()}><svg class="h-5 w-5 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg></button>
+                </span>}
+            <div className="grid grid-cols-4 gap-6 grid-flow-row justify-items-center mt-10">
+                {filteredRestaurants.length !== 0
+                    ? filteredRestaurants?.map((restaurant) => (
+                        <Link key={restaurant.info.id} to={`/restaurant/${restaurant.info.id}`} className="filteredLink">
+                            {restaurant?.info?.aggregatedDiscountInfoV3?.discountTag === "FLAT DEAL" ? (
+                                <WithRestaurantCard resData={restaurant} key={restaurant.info.id} />
+                            ) : (
+                                <RestaurantCard resData={restaurant} key={restaurant.info.id} />
+                            )}
+                        </Link>
+                    ))
+                    : "No results match your search. Try another term!"}
+            </div>
+        </div>
+    );
+};
 
 export default Body;
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearCart } from '../utils/cartSlice.js';
 import ItemsList from './ItemsList.js';
+import { Link } from 'react-router-dom';
+import emptyCart from '../images/emptyCart.png'
 
 const Cart = () => {
     const cartItems = useSelector((store) => store.cart.items);
@@ -12,19 +14,25 @@ const Cart = () => {
     };
 
     return (
-        <div className='text-center m-4 p-4'>
-            <h1 className='font-bold text-xl'>Cart</h1>
-            <button className='py-1 px-4 m-2 bg-white text-black border-2  hover:bg-slate-200 rounded-lg mb-5' onClick={handleClearCart}>
-                Clear
-            </button>
+        <div className='text-center m-4 p-4 flex flex-col items-center justify-center h-[50vh]'>
             {cartItems.length !== 0 ? (
-                <div className='m-auto w-1/2 p-3 border-b-2 bg-gray-100 rounded-lg mb-6 shadow-md'>
-                    <ItemsList items={cartItems} />
-                </div>
+                <>
+                    <h1 className='font-bold text-xl'>Cart</h1>
+                    <button className='py-1 px-4 m-2 bg-[#ff4d4d] text-white  hover:bg-[#ff3333] rounded-lg mb-5' onClick={handleClearCart}>
+                        Clear
+                    </button>
+                    <div className='m-auto w-1/2 p-3 border-b-2 bg-gray-100 rounded-lg mb-6 shadow-md'>
+                        <ItemsList items={cartItems} />
+                    </div>
+                </>
             ) : (
-                <div className='text-xl font-bold p-6'>
-                    Cart empty. Add items to the cart.
-                </div>
+                <>
+                    <img src={emptyCart} alt="empty cart" width="100" />
+                    <div className='text-2xl font-bold p-6'>
+                        Your cart is <span className='text-red-500'>Empty!</span>
+                    </div>
+                    <Link to="/"><button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-3xl cursor-pointer'>Return to shop</button></Link>
+                </>
             )}
         </div>
     );
