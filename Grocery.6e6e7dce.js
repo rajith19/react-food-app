dd6eb6dbd8b603944aff2d5d21de6876585b289e
rajith19 <rajith19@users.noreply.github.com>
2024-10-12 01:06:45 +0000

    deploy: bcf5712f7ca50a6a58803b526c99010dfaa7ca2f

---
+++ Grocery.6e6e7dce.js
@@ -0,0 +1,3 @@
+var e=globalThis.parcelRequire164e;(0,e.register)("loR3J",function(r,a){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0");e("acw62");var l=()=>/*#__PURE__*/(0,i.jsx)("div",{children:/*#__PURE__*/(0,i.jsx)("h1",{children:"Grocery"})})});//# sourceMappingURL=Grocery.6e6e7dce.js.map
+
+//# sourceMappingURL=Grocery.6e6e7dce.js.map
